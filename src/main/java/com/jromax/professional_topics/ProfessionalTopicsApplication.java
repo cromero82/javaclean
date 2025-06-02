170d2256cf579f71f3bf29d8c591b3e1e56dc3b7
Carlos romero <soportecarlosromero@gmail.com>
2025-06-01 22:37:25 -0500

    health controller

---
+++ src/main/java/com/jromax/professional_topics/ProfessionalTopicsApplication.java
@@ -0,0 +1,13 @@
+package com.jromax.professional_topics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessionalTopicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessionalTopicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jromax/professional_topics/ProfessionalTopicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jromax.professional_topics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessionalTopicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
